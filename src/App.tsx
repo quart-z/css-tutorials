f29a255ee90b8c3f89e33f82298e15fba4b77911
quart-z <jtw2212@gmail.com>
2022-09-22 23:48:15 -0700

    Main page, general setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function Home() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <body>
+      <header>
+
       </header>
-    </div>
+      <div className = "main-content">
+      {/* Section 1 - Content section , <Content /> */}
+
+      </div>
+    </body>
   );
 }
 
-export default App;
+export default Home;
+
