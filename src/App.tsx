629cae757a03bb7b74fdd544b93ace9a2e5aea5c
quart-z <jtw2212@gmail.com>
2022-10-05 23:33:21 -0700

    Added Initial Box for Tutorial
    - Added temp box
    - Adjusted HTML Syntax for organization
    - Added styling
    - Tutorial-grid, tutorial-box, tutorial-box-text

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,17 @@ function Home() {
       </header>
       <div className = "main-content-box">
       {/* Section 1 - Content section , <Content /> */}
+
         <div className = "gradient-container">
           <div className="gradient-noise"> </div>
-          <div className = "gradient1">
+          <div className = "gradient1"> </div>
+        </div>
+
+        <div className = "tutorial-grid">
+          <div className = "tutorial-box"> 
+            <h1 className = "tutorial-box-text"> CSS Tutorials </h1>
           </div>
+
         </div>
       </div>
     </body>
